e4a63681ec63487413b0c5083d48632cad3efec9
Hayoun Oh <hayounoh@g.harvard.edu>
2020-10-21 19:12:34 -0400

    Adding a null test

--- test_roots.py
+++ test_roots.py
@@ -22,3 +22,6 @@ def test_linearroots_types():
 def test_linearroots_zerocoeff():
     with pytest.raises(ValueError):
         roots.linear_roots(a=0.0)
+
+def null_test():
+    assert (True == True)
